2f4746065059e69588632979e9f550222fb700bd
rubikabot <102610040+rubikabot@users.noreply.github.com>
2022-04-09 16:59:59 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -667,7 +667,7 @@ def get_backup(text,chat,bot):
 
 g_usvl = ''
 test_usvl = ''
-auth = "rdzryjswfkhkathhdvzjtqhxberkttyk"
+auth = "kgerbzgyvtumlwmbzasaluddbrhaivkp"
 bot = Bot(auth)
 list_message_seened = []
 time_reset = random._floor(datetime.datetime.today().timestamp()) + 350
